d759a550e9159e3f4187e9bc57529f1e1b2be651
Hammad Ali <hammadaali02@gmail.com>
2024-08-13 23:44:47 +0500

    completed

--- lib/Presentation/Declarations/Constants/Images/image_files.dart
+++ lib/Presentation/Declarations/Constants/Images/image_files.dart
@@ -1,10 +1,7 @@
 List imagepath = [
-  // 'assets/Images/card_1.png',
-  // 'assets/Images/card_3.png',
-  // 'assets/Images/card_2.png',
+  'assets/Images/card_1.png',
+  'assets/Images/card_3.png',
+  'assets/Images/card_2.png',
   'assets/Images/card_4.png',
   'assets/Images/card_5.png',
-  'assets/Images/card_6.png',
-  'assets/Images/card_7.png',
-  'assets/Images/card_8.png',
 ];
